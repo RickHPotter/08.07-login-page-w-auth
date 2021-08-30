94d5466b293f86a84d0690b25d1b6c52b0e16ecc
RickHPotter <luisfla55@hotmail.com>
2021-08-30 14:57:20 -0300

    nuke and reupload

--- src/config/globalConfig.js
+++
@@ -1,6 +0,0 @@
-import Axios from 'axios'
-export const rootUrl = 'https://localhost/'
-
-export const Http = Axios.create({
-    baseURL: rootUrl
-})
--- store/store.js
+++
@@ -1,7 +0,0 @@
-import { createStore, applyMiddleware } from 'redux'
-import ReduxThunk from 'redux-thunk'
-import rootReducer from './reducers/rootReducer'
-
-let middleWare = [ReduxThunk]
-
-export const store = createStore(rootReducer, applyMiddleware(...middleWare));
